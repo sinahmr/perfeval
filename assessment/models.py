597bdac887f507041bb4fc12cd17fa30a8feab99
amirhoseinsa <amirsfr53@gmail.com>
2018-06-19 18:25:08 +0430

    models in assessment

--- assessment/models.py
+++ assessment/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+from authentication.models import Employee
+
+class Season(models.Model):
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+
+class QualitativeCriterion(models.Model):
+    choices = models.CharField(verbose_name='انتخاب ها', max_length=20, null=False, blank=False)
+    interpretation = models.TextField()
+
+class QuantitativeCriterion(models.Model):
+    formula = models.CharField(verbose_name='انتخاب ها', max_length=20, null=False, blank=False)
+    interpretation = models.TextField()
+
+class Scale(models.Model):
+    title = models.CharField(verbose_name='عنوان', max_length=20, null=False)
+    description = models.TextField()
+    qualitativeCriterion = models.ForeignKey(QualitativeCriterion)
+    quantitativeCriterion = models.ForeignKey(QuantitativeCriterion)
+
+class Assessment(models.Model):
+    assessor = models.ForeignKey(Employee, related_name= "assessments_as_assessor" , related_query_name="assessments_as_assessor" ,
+                                 null=False, blank= False)
+    assessed = models.ForeignKey(Employee, related_name= "assessments_as_assessed" , related_query_name="assessments_as_assessed" ,
+                                 null=False, blank= False)
+    season = models.ForeignKey(Season, related_name="assessments", related_query_name="assessments" , null=False , blank= False)
+
+    unique_together = ("assessor", "assessed", "season")
+
+    def __str__(self):
+        return self.name
+
+class ScaleAnswer(models.Model):
+    assessment = models.ForeignKey(Assessment)
+    scale = models.OneToOneField(Scale, null=False)
+    qualitativeAnswer = models.CharField()
+    quantitativeAnswer = models.CharField()
+
+class PunnishmentReward(models.Model):
+    method = models.TextField(null=False, blank=False)
+    type = models.BooleanField(null=True)
+    season = models.ForeignKey(Season, related_name="assessments", related_query_name="assessments", null=False,
+                               blank=False)
+    employee = models.ForeignKey(Employee)
+
--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from . import models
+
+admin.site.register(models.Unit)
+# admin.site.register(models.UserManager)
+admin.site.register(models.User)
+admin.site.register(models.GeneralAdmin)
+admin.site.register(models.Employee)
+admin.site.register(models.UnitAdmin)
+
