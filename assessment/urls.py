ee558549be88bd7c0581f27587017ebffc7c4c0b
emad <khaleghpanah@gmail.com>
2018-06-21 17:41:10 +0430

    commit -am

--- assessment/urls.py
+++ assessment/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
      path('employee/list/', views.EmployeesListView.as_view(), name='employee_list'),
      path('employee/show/<int:pk>/', views.ShowEmployeeView.as_view(), name='show_employee'),
      path('employee/home/', views.ShowMyDetailsView.as_view(), name='show_my_details'),
-     path('assessment/list/', views.AssessedsListView.as_view(), name='assessment_list'),
-     path('assessment/<int:pk>', views.AssessorsListView.as_view(), name='assessors'),
+     path('eval/list/', views.AssessedsListView.as_view(), name='assessment_list'),
+     #path('eval/<int:pk>', views.AssessorsListView.as_view(), name='assessors'),
 ]
