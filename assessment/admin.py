1acca8f6b20c1f2ee264f0d70a76417eb9f87aa9
amirhoseinsa <amirsfr53@gmail.com>
2018-06-21 22:51:35 +0430

    remove useless comments

--- assessment/admin.py
+++ assessment/admin.py
@@ -4,7 +4,7 @@ from . import models
 admin.site.register(models.Assessment)
 admin.site.register(models.ScaleAnswer)
 admin.site.register(models.Scale)
-admin.site.register(models.PunnishmentReward)
+admin.site.register(models.PunishmentReward)
 admin.site.register(models.QuantitativeCriterion)
 admin.site.register(models.QualitativeCriterion)
 admin.site.register(models.Season)
