8493a5084ef7c4e2440a8a8f3096b82616ac420b
Sina Hajimiri <sina.hajimiri@gmail.com>
2018-07-10 00:52:41 +0430

    Fix validation removal

--- perfeval/settings.py
+++ perfeval/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     'assessment.apps.AssessmentConfig',
-    'validation.apps.ValidationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
